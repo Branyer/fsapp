51de7eb8e9eb0d2a9ad6d6543557d0b08f224a4b
Branyer Vergara <Branyervergarac@gmail.com>
2021-09-01 10:31:42 -0400

    adding routing

--- App.js
+++ App.js
@@ -1,28 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View } from 'react-native';
 import { withAuthenticator } from 'aws-amplify-react-native'
 import Amplify from 'aws-amplify'
 import config from './src/aws-exports'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './components/Home';
+import Project from './components/Project';
+
+const Stack = createNativeStackNavigator()
+
+
 Amplify.configure(config)
 
+
  function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Project" component={Project} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
-});
+ 
+})
 
-
-export default withAuthenticator(App)
+// export default withAuthenticator(App)
+export default App
---
+++ components/Home.js
@@ -0,0 +1,103 @@
+import React, {useState} from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import ProjectTitle from './ProjectTitle';
+
+
+ function Home() {
+
+  const [project, setProject] = useState();
+  const [projectItems, setProjectItems] = useState([{id: "0", title: "Project1"}]);
+
+  const handleAddProject = () => {
+    Keyboard.dismiss();
+    setProjectItems([...projectItems, { id: projectItems.length + 1 , title: project}])
+    setProject(null);
+  }
+
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1
+        }}
+        keyboardShouldPersistTaps='handled'
+      >
+        {/* Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Projects</Text>
+        <View style={styles.items}>
+          {projectItems.map(item => {
+
+              return <ProjectTitle key={item.id} text={item.title}/>
+
+          })}
+        </View>
+      </View>
+      </ScrollView>
+
+      {/* Write a task */}
+      {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
+      <KeyboardAvoidingView 
+        behavior={"height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Add a Project'} value={project} onChangeText={text => setProject(text)} />
+        <TouchableOpacity onPress={() => handleAddProject()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {},
+})
+
+// export default withAuthenticator(App)
+export default Home
